c170f230dcc1cf5bfb8bbdc4fb9950e1090d9305
GuidoCostaDF <guidocostadf@gmail.com>
2022-08-31 11:17:07 -0300

    serviço de upload

--- gatitobook/src/app/animais/animais-routing.module.ts
+++ gatitobook/src/app/animais/animais-routing.module.ts
@@ -1,3 +1,4 @@
+import { NovoAnimalComponent } from './novo-animal/novo-animal.component';
 import { ListaAnimaisResolver } from './lista-animais/lista-animais.resolver';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
@@ -9,9 +10,14 @@ const routes: Routes = [
     path: '',
     component: ListaAnimaisComponent,
     resolve: {
-      animais: ListaAnimaisResolver
-    }
+      animais: ListaAnimaisResolver,
+    },
   },
+  {
+    path: 'novo',
+    component: NovoAnimalComponent,
+  },
+
   {
     path: ':animalId',
     component: DetalheAnimalComponent,
